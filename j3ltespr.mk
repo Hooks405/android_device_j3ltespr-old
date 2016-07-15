d3b9684848e3610be57753c8353c3f9bef76f25f
grace5921 <androidlover5842@gmail.com>
2016-07-15 22:02:45 +0530

    typos

--- j3ltespr.mk
+++ j3ltespr.mk
@@ -15,10 +15,10 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Also get non-open-source specific aspects if available
-$(call inherit-product-if-exists, vendor/samsung/a5-common/a5-common-vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/j3ltespr/j3ltespr-vendor.mk)
 
 # Common overlay
-DEVICE_PACKAGE_OVERLAYS += device/samsung/a5-common/overlay
+DEVICE_PACKAGE_OVERLAYS += device/samsung/j3ltespr/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
