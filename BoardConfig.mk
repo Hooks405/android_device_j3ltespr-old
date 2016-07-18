f6f226100159492485ac6fa3b7157b0338029b29
grace5921 <androidlover5842@gmail.com>
2016-07-18 22:31:58 +0530

    add ota Assert

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,6 +38,9 @@ TARGET_CPU_CORTEX_A53 := true
 
 TARGET_SPECIFIC_HEADER_PATH := device/samsung/j3ltespr/include
 
+# Assert
+TARGET_OTA_ASSERT_DEVICE := j3ltespr,SM-J320P,J320P
+
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK      := device/samsung/j3ltespr/mkbootimg.mk
 BOARD_KERNEL_CMDLINE         := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci androidboot.selinux=permissive
