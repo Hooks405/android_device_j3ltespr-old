030696d1027ceca2812920ebe9dd707638560b9a
grace5921 <androidlover5842@gmail.com>
2016-07-19 21:21:12 +0530

    test without custom mkbootimg

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,7 +42,7 @@ TARGET_SPECIFIC_HEADER_PATH := device/samsung/j3ltespr/include
 TARGET_OTA_ASSERT_DEVICE := j3ltespr,SM-J320P,J320P
 
 # Kernel
-BOARD_CUSTOM_BOOTIMG_MK      := device/samsung/j3ltespr/mkbootimg.mk
+#BOARD_CUSTOM_BOOTIMG_MK      := device/samsung/j3ltespr/mkbootimg.mk
 BOARD_KERNEL_CMDLINE         := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci androidboot.selinux=permissive
 BOARD_KERNEL_BASE            := 0x80000000
 BOARD_RAMDISK_OFFSET         := 0x02000000
